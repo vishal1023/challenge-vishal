5238cd2dd35694a715effe94538ac7ad2c1e3b2e
Karim Omar <karim.omar@tooltime.de>
2022-03-09 19:44:52 +0100

    Rename bff controller

--- web-bff/src/main/kotlin/de/tooltime/webbff/controller/WebBffController.kt
+++ web-bff/src/main/kotlin/de/tooltime/webbff/controller/WebBffController.kt
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class UserController(
+class WebBffController(
         val userService: UserService,
         val timeTrackingService: TimeTrackingService
 ) {
