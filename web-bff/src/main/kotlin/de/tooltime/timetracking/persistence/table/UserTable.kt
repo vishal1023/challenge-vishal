9aaef2e4a242e4c685b7473529cacd772c06d2c7
Karim Omar <karim.omar@tooltime.de>
2022-03-09 19:38:07 +0100

    Delete user table and mapper from bff

--- web-bff/src/main/kotlin/de/tooltime/timetracking/persistence/table/UserTable.kt
+++
@@ -1,8 +0,0 @@
-package de.tooltime.timetracking.persistence.table
-
-import org.jetbrains.exposed.dao.id.UUIDTable
-
-object UserTable : UUIDTable("user") {
-    val firstName = text("first_name")
-    val lastName = text("last_name")
-}
